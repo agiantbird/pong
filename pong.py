6ca6603557679a2f3be6e9779b08d97da9c01a75
Rich Hoyt <rhoyt10@gmail.com>
2022-05-12 20:27:50 -0400

    First commit

---
+++ pong.py
@@ -0,0 +1,12 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Pong by agiantbird")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+#Main game loop
+
+while True:
+    window.update()
